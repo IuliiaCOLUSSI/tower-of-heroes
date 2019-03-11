9e8ee8e9cf49a6481200d9291f2bcda91b4c53a4
IuliiaCOLUSSI <45996466+IuliiaCOLUSSI@users.noreply.github.com>
2019-03-11 16:31:20 +0100

    play with app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  heroes = [
+    'Superman',
+    'Wonderwoman',
+    'Prettywoman',
+  ];
+
+  name = '';
+  heroToAdd = '';
+
+  addHero() {
+    // this.heroes.push(this.heroToAdd);
+    if (this.heroToAdd.trim().length > 0) { 
+      this.heroes.push(this.heroToAdd.trim());
+      this.heroToAdd = '';
+     
+    }else{
+        alert('le nom de Heroes ne doit pas restée vide');
+        }
+
+    // if (this.heroToAdd.trim() == heroToAdd) { 
+    //   this.heroes.pull(this.heroToAdd.trim());
+     
+    // }else{
+    //     alert('le nom de Heroes déjà existe');
+    //     }
+      // si heroToAdd = 'Coucou';
+      // alors heroToAdd.trim() = 'coucou';
+      // donc si heroToAdd = '         ';
+      // alors heroToAdd.trim() = ''.
+
+
+  };
+
+
+
+
+
+      // < script >
+      // function check() {
+
+      //   if (heroToAdd.length != 0) {
+      //     $('#button').removeAttr('disabled');
+      //   } else {
+      //     $('#button').attr('disabled', 'disabled');
+      //   }
+      // }
+      // < /script>
+
+
 }
+
